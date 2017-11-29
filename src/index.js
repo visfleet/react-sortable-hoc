296a60474212320c63e5f4223123745a8ce744af
Ryan Williams <ryan.williams@visfleet.com>
2017-11-30 11:29:20 +1300

    Export SortableGroup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 export SortableContainer from './SortableContainer';
 export SortableElement from './SortableElement';
 export SortableHandle from './SortableHandle';
+export SortableGroup from './SortableGroup';
 
 export sortableContainer from './SortableContainer';
 export sortableElement from './SortableElement';
 export sortableHandle from './SortableHandle';
+export sortableGroup from './SortableGroup';
 
 export {arrayInsert,arrayMove} from './utils';
 export DragLayer from './DragLayer';
